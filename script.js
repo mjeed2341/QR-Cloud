bc853027ed0dd78a9070446c19af74bfbe4a20be
mjeed2341 <abd234167@gmail.com>
2025-11-19 22:17:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let files=[];
 
+// اكتشاف نوع الملف وإرجاع أيقونة
+function getFileIcon(type){
+    if(type.includes("pdf")) return "https://img.icons8.com/color/48/000000/pdf.png";
+    if(type.includes("word") || type.includes("doc")) return "https://img.icons8.com/color/48/000000/ms-word.png";
+    if(type.includes("sheet") || type.includes("excel") || type.includes("xls")) return "https://img.icons8.com/color/48/000000/ms-excel.png";
+    if(type.includes("powerpoint")) return "https://img.icons8.com/color/48/000000/ms-powerpoint.png";
+    if(type.includes("image")) return "https://img.icons8.com/color/48/000000/image.png";
+    if(type.includes("video")) return "https://img.icons8.com/color/48/000000/video.png";
+    if(type.includes("audio")) return "https://img.icons8.com/color/48/000000/audio.png";
+    return "https://img.icons8.com/color/48/000000/file.png";
+}
+
 function handleUpload(){
   let f=document.getElementById('fileInput').files[0];
   if(!f) return alert("اختر ملف");
@@ -13,11 +25,42 @@ function render(){
   files.forEach((f,i)=>{
     c.innerHTML+=`
       <div class='file-card'>
-        <b>${f.name}</b> (${f.type})
+        <img class="file-icon" src="${getFileIcon(f.type)}">
+        <b>${f.name}</b>
+        <button onclick="viewFile(${i})">عرض</button>
+        <button onclick="editFile(${i})">تعديل</button>
         <button onclick="makeQR(${i})">QR</button>
-        <button onclick="openFile(${i})">فتح</button>
       </div>`;
   });
+}
+
+// عرض الملف داخل التطبيق
+function viewFile(i){
+    const file = files[i];
+    const url = URL.createObjectURL(file);
+    const viewer = document.getElementById("viewerFrame");
+
+    // PDF يعمل مباشر
+    if(file.type.includes("pdf")){
+        viewer.src = url;
+    } 
+    else {
+        // Word - Excel - PowerPoint
+        viewer.src = "https://docs.google.com/gview?embedded=true&url=" + url;
+    }
+
+    viewer.style.display = "block";
+    document.getElementById("closeViewer").style.display = "block";
+}
+
+// وضع التعديل
+function editFile(i){
+    alert("ميزة التعديل سوف يتم تطويرها في النسخة القادمة ❤️");
+}
+
+function closeViewer(){
+    document.getElementById("viewerFrame").style.display = "none";
+    document.getElementById("closeViewer").style.display = "none";
 }
 
 function makeQR(i){
@@ -41,9 +84,4 @@ function downloadQR(){
   link.download = "qr.png";
   link.href = canvas.toDataURL();
   link.click();
-}
-
-function openFile(i){
-  let url=URL.createObjectURL(files[i]);
-  window.open(url,"_blank");
 }
